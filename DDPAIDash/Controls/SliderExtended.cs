13d2e53091a9e633aff9c71fe29f073c35047c6a
DerekGn <9FB2EFB6AF41A7C3>
2017-04-01 17:49:46 +0100

    Added SliderExtended that prevents updating binding while slider moving

---
+++ DDPAIDash/Controls/SliderExtended.cs
@@ -0,0 +1,40 @@
+﻿
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+
+namespace DDPAIDash.Controls
+{
+    public class SliderExtended : Slider
+    {
+        private Binding SupressedBinding { get; set; }
+
+        protected override void OnPointerEntered(PointerRoutedEventArgs e)
+        {
+            base.OnPointerEntered(e);
+            
+            var expression = GetBindingExpression(ValueProperty);
+
+            if (expression != null)
+            {
+                SupressedBinding = expression.ParentBinding;
+                SetBinding(ValueProperty, new Binding());
+                SetValue(ValueProperty, Value);
+            }
+        }
+
+        protected override void OnPointerExited(PointerRoutedEventArgs e)
+        {
+            if (SupressedBinding != null)
+            {
+                var value = Value;
+                SetBinding(ValueProperty, SupressedBinding);
+                SetValue(ValueProperty, value);
+                SupressedBinding = null;
+            }
+
+            base.OnPointerExited(e);
+        }
+    }
+}
